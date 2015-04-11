cc130bb506f0733eaef1346094ed07b510431109
stedes <stevendesair@gmail.com>
2015-04-11 12:58:16 +0200

    working {{each}}

--- meteor_first_tut.js
+++ meteor_first_tut.js
@@ -1,23 +1,22 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+// Create a MongoDB Collection
+PlayersList = new Mongo.Collection('players');
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+
+if(Meteor.isClient){
+  console.log("Hello client");
+
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    }
+    ,
+    'otherHelper': function(){
+      return "Something else"
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
+};
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+if(Meteor.isServer){
+  console.log("Hello server");
+};

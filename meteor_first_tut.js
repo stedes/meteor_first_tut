12b2cf1ed82b994a18934b3f5c03172df2499b7b
stedes <stevendesair@gmail.com>
2015-04-11 18:36:00 +0200

    working adding player removing player

--- meteor_first_tut.js
+++ meteor_first_tut.js
@@ -7,13 +7,18 @@ if(Meteor.isClient){
 
   Template.leaderboard.helpers({
     'player': function(){
-      return PlayersList.find({},{sort: {score:-1}});
+      return PlayersList.find({},{sort: {score:-1, name:1}});
     }
     ,
     'selectedClass': function(){
       var playerId = this._id;
-      var selectedPlayer = Session.get('selectedPlayer')
+      var selectedPlayer = Session.get('selectedPlayer');
       if(playerId === selectedPlayer){ return "selected"};
+    }
+    ,
+    'showSelectedPlayer': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne(selectedPlayer);
     }
   });
 
@@ -31,6 +36,23 @@ if(Meteor.isClient){
     'click .decrement': function(){
       var selectedPlayer = Session.get('selectedPlayer');
       PlayersList.update(selectedPlayer, {$inc: {score: -5}});
+    }
+    ,
+    'click .remove': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.remove(selectedPlayer);
+    }
+  });
+
+  Template.addPlayerForm.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var playerNameVar = event.target.playerName.value;
+      PlayersList.insert({
+        name: playerNameVar,
+        score: 0
+      });
+      event.target.playerName.value = '';
     }
   });
 
